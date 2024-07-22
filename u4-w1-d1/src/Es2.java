8d0e9fd4db01002b75a120b7a14952a5cbfffa08
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-07-22 16:52:27 +0200

    completato secondo esercizio

--- u4-w1-d1/src/Es2.java
+++ u4-w1-d1/src/Es2.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Es2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Inserisci il tuo nome: ");
+        String primaStringa = sc.nextLine();
+
+        System.out.println("Inserisci il tuo cognome: ");
+        String secondaStringa = sc.nextLine();
+
+        System.out.println("Inserisci il tuo soprannome: ");
+        String terzaStringa = sc.nextLine();
+
+        String stringheConcatenate = primaStringa + secondaStringa + terzaStringa;
+        System.out.println("Stringhe concatenate: " + stringheConcatenate);
+
+        String stringheConcatenateInverso = terzaStringa + secondaStringa + primaStringa;
+        System.out.println("Stringhe concatenate inversamente: " + stringheConcatenateInverso);
 
     }
 
-    
+
 }
